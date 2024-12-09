9fd75d36cf6b593f9e2d846f4980cf8b68181d63
yurialv <yuribarbearia@icloud.com>
2024-12-09 17:47:42 +0000

    modifica a pagina

--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,5 @@
 function Home () {
-    return <h1>Livianne eu amo você, se você me ama da uma risadinha.</h1>
+    return <h1>Livianne eu amo você, se você me ama da uma risadinha! <3</h1>
 }
 
 export default Home;
