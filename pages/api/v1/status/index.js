577b0df37b35d78861bd0a58bfb6961f17021ae3
yurialv <yuribarbearia@icloud.com>
2025-04-02 00:58:07 +0000

    add `compose` config file with database service

--- pages/api/v1/status/index.js
+++ pages/api/v1/status/index.js
@@ -1,10 +1,5 @@
-import database from "../../../../infra/database";
-
-async function status(request, response) {
-  const result = await database.query("SELECT 1+1 as sum");
-  console.log(result.rows); // Verifique o retorno da query
-
-  return response.status(200).json({ message: "TESTE TABNEWS OLÁ A TODOS" });
+function status(request, response) {
+  response.status(200).json({ chave: "São acima da méida" });
 }
 
 export default status;
